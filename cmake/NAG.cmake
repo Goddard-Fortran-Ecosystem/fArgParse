7c948089953d3c86d38a459a26d69987fb4a9000
Tom Clune <tom@tom-clunes-imac.home>
2019-01-10 08:27:53 -0500

    Preparing for open source release.
    - Added COPYRIGHT and LICENSE.pdf
    - Added cmake subdirectory - missed in earlier commits.

---
+++ cmake/NAG.cmake
@@ -0,0 +1,14 @@
+# Compiler specific flags for NAG Fortran compiler
+
+set(traceback "-gline")
+#set(check_all "-C=all")
+set(check_all "-C=array -C=alias -C=bits -C=calls -C=do -C=intovf -C=present -C=pointer")
+
+set(cpp "-fpp")
+set(mismatch "-mismatch")
+
+set(CMAKE_Fortran_FLAGS_DEBUG  "-O0")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
+set(CMAKE_Fortran_FLAGS "-g ${cpp} ${traceback} ${check_all} -w=x95 -nocheck_modtime")
+
+add_definitions(-D_NAG)
