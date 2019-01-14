acd9cc1a32355f942ada05be14890458b74c2fad
Tom Clune <tlclune@gmail.com>
2019-01-14 16:28:35 -0500

    Making gFTL naming consistent across projects.

--- cmake/FindGFTL.cmake
+++ cmake/FindGFTL.cmake
@@ -1,11 +1,11 @@
-if (FTL) # use default
-  set(ftl_install_dir ${FTL})
+if (GFTL) # use default
+  set(gftl_install_dir ${GFTL})
 else()
-  set(ftl_source_dir ${CMAKE_CURRENT_SOURCE_DIR}/FTL)
-  set(ftl_install_dir ${CMAKE_CURRENT_BINARY_DIR}/FTL/install)
+  set(gftl_source_dir ${CMAKE_CURRENT_SOURCE_DIR}/gFTL)
+  set(gftl_install_dir ${CMAKE_CURRENT_BINARY_DIR}/gFTL/install)
 
   include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
-  file(GLOB all_files ${ftl_source_dir}/*)
+  file(GLOB all_files ${gftl_source_dir}/*)
   list(LENGTH all_files n_files)
 
   if(n_files LESS_EQUAL 3)
@@ -23,10 +23,10 @@ else()
     GIT_REPOSITORY ${repository}
     DOWNLOAD_COMMAND ${download_command}
     UPDATE_COMMAND ${update_command}
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FTL
-    SOURCE_DIR ${ftl_source_dir}
-    INSTALL_DIR ${ftl_install_dir}
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gFTL
+    SOURCE_DIR ${gftl_source_dir}
+    INSTALL_DIR ${gftl_install_dir}
     BUILD_COMMAND make
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${ftl_install_dir}  -DCMAKE_INSTALL_MESSAGE=LAZY
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${gftl_install_dir}  -DCMAKE_INSTALL_MESSAGE=LAZY
     INSTALL_COMMAND make install)
 endif()
