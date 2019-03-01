e83f07d01cbd62d7eb4e89bf469fdc068a1ee7b3
Tom Clune <tom.clune@nasa.gov>
2019-03-01 10:59:01 -0500

    Workarounds for ifort and gfortan.
    A use case in pFUnit 4.0 detected problems in the use of
    StringUnlimitedMap in fArgParse.  A reproducer (below) and workaround
    were submitted to intel.  That exposed another known issue for
    gfortran for which a workaround (deepcopy()) was introduced.  No
    reproducer has been submitted to gfortran (no energy).
    
    The code below works with ifort 19.1 _if_ the commented interface
    block is uncommented.  Otherwise it produces garbage in the output
    string.
    
    module fp_String
      implicit none
      private
    
      public :: String
      type :: String
         character(:), allocatable :: string
      end type String
    
    !!$  interface String
    !!$     module procedure new_string
    !!$  end interface String
    
    contains
    
       function new_string(s) result(str)
          type (String) :: str
          character(*), intent(in) :: s
          str%string = s
       end function new_string
    
    end module fp_String
    
    module fp_SU_map
       use fp_String
       implicit none
       private
    
       public :: SU_map
    
       type SU_map
          class(*), allocatable :: component
       end type SU_map
    
       public :: get_defaults2
    
    contains
    
       subroutine get_defaults2(option_values)
          type (SU_Map), intent(out) :: option_values
          class(*), allocatable :: q
    
          q = 'TestRunner'
          select type (q)
          type is (character(*))
             option_values%component = String(q)
          end select
    
       end subroutine get_defaults2
    
    end module fp_SU_map
    
    program main
       use fp_SU_map
       use fp_String
       type (SU_map) :: options
    
       call get_defaults2(options)
       select type (opt => options%component)
       class is (String)
          print*,'String...... ',opt%string
          print*,'should be... TestRunner'
       end select
    
    end program main

--- src/String.F90
+++ src/String.F90
@@ -9,4 +9,18 @@ module fp_String
      character(:), allocatable :: string
   end type String
 
+  interface String
+     module procedure new_string
+  end interface String
+
+contains
+
+   function new_string(s)
+      type (String) :: new_string
+      character(*), intent(in) :: s
+      new_string%string = s
+   end function new_string
+
 end module fp_String
+
+
