eedf34f8303e2f313c5badaa582b2ca29719c5b8
Matthew Thompson <matthew.thompson@nasa.gov>
2019-11-08 05:58:42 -0800

    Edits to allow ifort 19.0.3 to build

--- src/fArgParse.F90
+++ src/fArgParse.F90
@@ -1,11 +1,7 @@
 module fArgParse
    use fp_ArgParser
-   use fP_CommandLineArguments
    use fp_Cast
    use gFTL_StringUnlimitedMap
-   use gFTL_StringVector
-   use gFTL_IntegerVector
-   use gFTL_RealVector
    implicit none
    private
 
