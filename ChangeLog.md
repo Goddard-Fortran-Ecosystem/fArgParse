62b3699422818a584c05fd5ca4ff089d94a8d645
Tom Clune <thomas.l.clune@nasa.gov>
2019-11-07 20:12:43 -0500

    Fixes #55 - build hang in ifort 19.0.3
    Workaround suggested by user
    
    Also:
      - Updating to latest gFTL and gFTL-shared.
    
        These have a workaround for a memory leak in ifort-18.  Unlikely
        to be an measureable issue for fArgParse, but so long as changes
        are being made anyway ...
    
      - updating travis script
        Necessary apparently.

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,7 @@
-0.9.0   2019-09-04
-     - Updated to use pFUnit 4.0
+0.9.0 2019-09-04
+      - Updated to use pFUnit 4.0
      
-     
+0.9.1 2019-11-08
+      - Workaround for compiler hang in ifort 19.0.3
+      - Updated gFTL for memory leak workaround in ifort 18
+
--- src/fArgParse.F90
+++ src/fArgParse.F90
@@ -1,17 +1,12 @@
 module fArgParse
    use fp_ArgParser
-   use fP_CommandLineArguments
    use fp_Cast
    use gFTL_StringUnlimitedMap
-   use gFTL_StringVector
-   use gFTL_IntegerVector
-   use gFTL_RealVector
    implicit none
+   private
 
    public :: ArgParser
-   public :: cast
    public :: StringUnlimitedMap
-   public :: get_command_line_arguments
-   public :: get_command_line_argument
+   public :: cast
 
 end module fArgParse
